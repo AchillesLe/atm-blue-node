6e7cfb12dff757a954e97b94ee7a95abe220e751
vfa-baolv <baolv@vitalify.asia>
2025-11-10 15:19:15 +0700

    update test 6

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,11 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.js'],
-  collectCoverageFrom: [
-    'src/**/*.js',
-    '!src/app.js'
-  ],
+  collectCoverageFrom: ['src/**/*.js'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
   verbose: true,
   setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
-  testTimeout: 30000
+  testTimeout: 30000,
+  silent: false,
 };
