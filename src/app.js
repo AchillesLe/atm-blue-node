af6028f0ad36c73947f1bda2186736600c961779
vfa-baolv <baolv@vitalify.asia>
2026-01-03 15:08:20 +0700

    update

--- src/app.js
+++ src/app.js
@@ -41,7 +41,13 @@ app.get('/', (req, res) => {
     processId: process.pid,
     instanceStartTime: INSTANCE_START_TIME,
     uptime: process.uptime(),
-    isEcsEnvironment: !!ECS_TASK_ID
+    isEcsEnvironment: !!ECS_TASK_ID,
+
+
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME
   });
 });
 
