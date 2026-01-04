fb003f3de70c72d75a683ec60ed2c21e856d8d74
vfa-baolv <baolv@vitalify.asia>
2026-01-04 21:39:00 +0700

    feat: update

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const APP_ENV = process.env.APP_ENV || 'local';
 
 async function getEcsTaskId() {
